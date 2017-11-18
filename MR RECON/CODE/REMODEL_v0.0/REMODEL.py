b706bb7fd3a9d13011a7c3131ef6c5ffb206dd91
Medical Imaging Research Center - DSI <sairam.geethanath@dayanandasagar.edu>
2017-11-18 14:22:47 +0530

    V.0.1

--- MR RECON/CODE/REMODEL_v0.0/REMODEL.py
+++ MR RECON/CODE/REMODEL_v0.0/REMODEL.py
@@ -8,13 +8,11 @@ Created on Mon Sep 11 10:46:35 2017
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
-#from sklearn.metrics import confusion_matrix
 import time
 from datetime import timedelta
 import math
 
-#from tf_utils1 import *
-#from fieldMaps import loadNaturalImages,loadTestImages
+
 
 #rest tf graph
 tf.reset_default_graph()
@@ -30,13 +28,11 @@ num_filters1 = 64         # There are 16 of these filters.
 filter_size2 = 5     # Convolution filters are 5 x 5 pixels.
 num_filters2 = 64         # There are 36 of these filters.
 
-# Convolutional Layer 2.
+# Convolutional Layer 3.
 filter_size3 = 5        # Convolution filters are 5 x 5 pixels.
 num_filters3 = 64         # There are 36 of these filters.
 
-# Convolutional Layer 2.
-filter_size4 = 5        # Convolution filters are 5 x 5 pixels.
-num_filters4 = 64         # There are 36 of these filters.
+
 
 
 img_size = 64
@@ -49,28 +45,26 @@ num_classes = img_size*img_size
 X_train_orig = np.load("X_train_orig_64.npy")
 Y_train_orig = np.load("Y_train_orig_64.npy")
 
-#X_test_orig = np.load("X_test_orig_64.npy")
-#Y_test_orig = np.load("Y_test_orig_64.npy")
-#X_train_orig,Y_train_orig = loadNaturalImages("D:\\Applications\\OffResonance\\Data\\ImageNET\\NaturalImages",img_size)
+X_test_orig = np.load("X_test_orig_64.npy")
+Y_test_orig = np.load("Y_test_orig_64.npy")
 
-#X_test_orig,Y_test_orig = loadTestImages("/Data/OffResonance/TestImages",img_size)
 
 
 
 print (X_train_orig.shape , Y_train_orig.shape)
 X_train_flatten = X_train_orig.reshape(X_train_orig.shape[0], -1).T
-#X_test_flatten = X_test_orig.reshape(X_test_orig.shape[0],-1).T
+X_test_flatten = X_test_orig.reshape(X_test_orig.shape[0], -1).T
 
-X_test = X_train_flatten
-Y_test = Y_train_orig
+X_test = X_test_flatten
+Y_test = Y_test_orig
 X_train = X_train_flatten
 Y_train = Y_train_orig
 
 print ("Shape of the X_train",X_train.shape)
 print ("Shape of the Y_train",Y_train.shape)
 
-print ("Shape of the X_train",X_test.shape)
-print ("Shape of the Y_train",Y_test.shape)
+print ("Shape of the X_test",X_test.shape)
+print ("Shape of the Y_test",Y_test.shape)
 
 
 
@@ -90,7 +84,7 @@ num_channels = 1
 
 def new_weights(shape, name):
     return tf.get_variable(name, shape, initializer=tf.contrib.layers.xavier_initializer())
-    #tf.Variable(tf.truncated_normal(shape, stddev=0.05))
+   
 
 def new_biases(length):
     return tf.Variable(tf.constant(0.05, shape=[length]))
@@ -115,15 +109,7 @@ def new_conv_layer(input,              # The previous layer.
     # Create new biases, one for each filter.
     biases = new_biases(length=num_filters)
 
-    # Create the TensorFlow operation for convolution.
-    # Note the strides are set to 1 in all dimensions.
-    # The first and last stride must always be 1,
-    # because the first is for the image-number and
-    # the last is for the input-channel.
-    # But e.g. strides=[1, 2, 2, 1] would mean that the filter
-    # is moved 2 pixels across the x- and y-axis of the image.
-    # The padding is set to 'SAME' which means the input image
-    # is padded with zeroes so the size of the output is the same.
+    
     layer = tf.nn.conv2d(input=input,
                          filter=weights,
                          strides=[1, 1, 1, 1],
@@ -160,8 +146,7 @@ def new_conv_layer(input,              # The previous layer.
     # but since relu(max_pool(x)) == max_pool(relu(x)) we can
     # save 75% of the relu-operations by max-pooling first.
 
-    # We return both the resulting layer and the filter-weights
-    # because we will plot the weights later.
+
     return layer, weights
 
 def new_deconv_layer(input,              # The previous layer.
@@ -170,8 +155,7 @@ def new_deconv_layer(input,              # The previous layer.
                    num_filters,        # Number of filters.
                    use_pooling=True,name = 'W'):  # Use 2x2 max-pooling.
 
-    # Shape of the filter-weights for the convolution.
-    # This format is determined by the TensorFlow API.
+ .
     shape = [filter_size, filter_size, num_input_channels, num_filters]
 
     # Create new weights aka. filters with the given shape.
@@ -180,15 +164,7 @@ def new_deconv_layer(input,              # The previous layer.
     # Create new biases, one for each filter.
     biases = new_biases(length=num_input_channels)
     outShape= tf.stack([tf.shape(x_image)[0],img_size,img_size,num_input_channels])
-    # Create the TensorFlow operation for convolution.
-    # Note the strides are set to 1 in all dimensions.
-    # The first and last stride must always be 1,
-    # because the first is for the image-number and
-    # the last is for the input-channel.
-    # But e.g. strides=[1, 2, 2, 1] would mean that the filter
-    # is moved 2 pixels across the x- and y-axis of the image.
-    # The padding is set to 'SAME' which means the input image
-    # is padded with zeroes so the size of the output is the same.
+    
     layer = tf.nn.conv2d_transpose(input,
                          filter=weights,
                          strides=[1, 1, 1, 1],
@@ -207,19 +183,9 @@ def new_deconv_layer(input,              # The previous layer.
     center=True,
     scale=True,
     is_training=training)
-    print(" bdecnn layer shape",cnn_bn.shape)
-    # Rectified Linear Unit (ReLU).
-    # It calculates max(x, 0) for each input pixel x.
-    # This adds some non-linearity to the formula and allows us
-    # to learn more complicated functions.
+   
     layer = tf.nn.relu(cnn_bn)
 
-    # Note that ReLU is normally executed before the pooling,
-    # but since relu(max_pool(x)) == max_pool(relu(x)) we can
-    # save 75% of the relu-operations by max-pooling first.
-
-    # We return both the resulting layer and the filter-weights
-    # because we will plot the weights later.
     return layer, weights
 
 def flatten_layer(layer):
@@ -356,33 +322,28 @@ def compute_cost(Z3, Y):
     """
     Computes the cost
     
-    Arguments:
-    Z3 -- output of forward propagation (output of the last LINEAR unit)
-    Y -- "true" labels vector placeholder, same shape as Z3
     
-    Returns:
-    cost - Tensor of the cost function
     """
     
     logits = tf.transpose(Z3)
     labels = tf.transpose(Y)
     cost = tf.reduce_mean(tf.square(logits-labels))
-    ### START CODE HERE ### (1 line of code)
+   
     #cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = labels))    
     regularizer2 = beta * tf.nn.l2_loss(weights_conv3)
     regularizer3 = beta * tf.nn.l2_loss(weights_conv2)
     regularizer4 = beta * tf.nn.l2_loss(weights_conv1)    
     
     cost = tf.reduce_mean(0.5*tf.square(logits-labels) +  regularizer2 + regularizer3 + regularizer4)    
-    #cost = tf.reduce_mean(tf.square(Z3-Y))        
+    
     return cost
 
 cost = compute_cost(y_pred_cls, y_true)
 
 
 
-optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
-optimizer1 = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
+
 
 
 
@@ -402,14 +363,11 @@ def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
     
     m = X.shape[1]                  # number of training examples
     mini_batches = []
-    #np.random.seed(seed)
-    
-    # Step 1: Shuffle (X, Y)
+  
     permutation = list(np.random.permutation(m))
     shuffled_X = X[:, permutation]
     shuffled_Y = Y[:, permutation].reshape((Y.shape[0],m))
 
-    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
     num_complete_minibatches = int(math.floor(m/mini_batch_size)) # number of mini batches of size mini_batch_size in your partitionning
     for k in range(0, num_complete_minibatches):
         mini_batch_X = shuffled_X[:, k * mini_batch_size : k * mini_batch_size + mini_batch_size]
@@ -417,7 +375,7 @@ def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
     
-    # Handling the end case (last mini-batch < mini_batch_size)
+   
     if m % mini_batch_size != 0:
         mini_batch_X = shuffled_X[:, num_complete_minibatches * mini_batch_size : m]
         mini_batch_Y = shuffled_Y[:, num_complete_minibatches * mini_batch_size : m]
@@ -437,11 +395,6 @@ def Evaluate_Train_Results():
     print ("Shape of the X_train Data",X_train.shape)    
     num_test = X_train.shape[1]
 
-    # Now calculate the predicted classes for the batches.
-    # We will just iterate through all the batches.
-    # There might be a more clever and Pythonic way of doing this.
-
-    # The starting index for the next batch is denoted i.
     i = 0
     
     while i < num_test:
@@ -466,7 +419,7 @@ def Evaluate_Train_Results():
         # end-index of the current batch.
         i = j
 
-    # Convenience variable for the true class-numbers of the test-set.
+    .
     print ("*****************************END***************************")
     print ("Shape of cls_pred_train",cls_pred_train.shape)
    
@@ -540,10 +493,9 @@ def optimize(num_iterations):
             # Run the optimizer using this batch of training data.
             # TensorFlow assigns the variables in feed_dict_train
             # to the placeholder variables and then runs the optimizer.
-            if i<5000:
-                _ , minibatch_cost = session.run([optimizer,cost], feed_dict=feed_dict_train)
-            else:
-                 _ , minibatch_cost = session.run([optimizer1,cost], feed_dict=feed_dict_train)
+            
+            _ , minibatch_cost = session.run([optimizer,cost], feed_dict=feed_dict_train)
+            
             
             epoch_cost += minibatch_cost / num_minibatches
             # Print the cost every 100 epoch
@@ -572,13 +524,13 @@ optimize(num_iterations=1)
 
 
 
-optimize(num_iterations=200) # We already performed 1 iteration above.
+optimize(num_iterations=600) # We already performed 1 iteration above.
 X_train = X_train
 Y_train = Y_train
 
 Evaluate_Train_Results()
 Evaluate_Test_Results()
-saver.save(session, "/Data/OffResonance/CNN/64x64_head_unseen/tmpDF400_100Kdf/model.ckpt")
+#saver.save(session, "/Data/OffResonance/CNN/64x64_head_unseen/tmpDF400_100Kdf/model.ckpt")
 #save_path = saver.save(session, "/tmp/model.ckpt")
 result = cls_pred_test[:,2:3] # To display 3rd image
 plt.imshow(result.reshape(img_size,img_size))
@@ -588,130 +540,6 @@ plt.imshow(result.reshape(img_size,img_size))
 
 
 
-def plot_conv_weights(weights, input_channel=0):
-    # Assume weights are TensorFlow ops for 4-dim variables
-    # e.g. weights_conv1 or weights_conv2.
-    
-    # Retrieve the values of the weight-variables from TensorFlow.
-    # A feed-dict is not necessary because nothing is calculated.
-    w = session.run(weights)
-
-    # Get the lowest and highest values for the weights.
-    # This is used to correct the colour intensity across
-    # the images so they can be compared with each other.
-    w_min = np.min(w)
-    w_max = np.max(w)
-
-    # Number of filters used in the conv. layer.
-    num_filters = w.shape[3]
-
-    # Number of grids to plot.
-    # Rounded-up, square-root of the number of filters.
-    num_grids = math.ceil(math.sqrt(num_filters))
-    
-    # Create figure with a grid of sub-plots.
-    fig, axes = plt.subplots(num_grids, num_grids)
-
-    # Plot all the filter-weights.
-    for i, ax in enumerate(axes.flat):
-        # Only plot the valid filter-weights.
-        if i<num_filters:
-            # Get the weights for the i'th filter of the input channel.
-            # See new_conv_layer() for details on the format
-            # of this 4-dim tensor.
-            img = w[:, :, input_channel, i]
-
-            # Plot image.
-            ax.imshow(img, vmin=w_min, vmax=w_max,
-                      interpolation='nearest', cmap='seismic')
-        
-        # Remove ticks from the plot.
-        ax.set_xticks([])
-        ax.set_yticks([])
-    
-    # Ensure the plot is shown correctly with multiple plots
-    # in a single Notebook cell.
-    plt.show()
-
-
-def plot_conv_layer(layer, image):
-    # Assume layer is a TensorFlow op that outputs a 4-dim tensor
-    # which is the output of a convolutional layer,
-    # e.g. layer_conv1 or layer_conv2.
-
-    # Create a feed-dict containing just one image.
-    # Note that we don't need to feed y_true because it is
-    # not used in this calculation.
-    feed_dict = {x: [image]}
-
-    # Calculate and retrieve the output values of the layer
-    # when inputting that image.
-    values = session.run(layer, feed_dict=feed_dict)
-
-    # Number of filters used in the conv. layer.
-    num_filters = values.shape[3]
-
-    # Number of grids to plot.
-    # Rounded-up, square-root of the number of filters.
-    num_grids = math.ceil(math.sqrt(num_filters))
-    
-    # Create figure with a grid of sub-plots.
-    fig, axes = plt.subplots(num_grids, num_grids)
-
-    # Plot the output images of all the filters.
-    for i, ax in enumerate(axes.flat):
-        # Only plot the images for valid filters.
-        if i<num_filters:
-            # Get the output image of using the i'th filter.
-            # See new_conv_layer() for details on the format
-            # of this 4-dim tensor.
-            img = values[0, :, :, i]
-
-            # Plot image.
-            ax.imshow(img, interpolation='nearest', cmap='binary')
-        
-        # Remove ticks from the plot.
-        ax.set_xticks([])
-        ax.set_yticks([])
-    
-    # Ensure the plot is shown correctly with multiple plots
-    # in a single Notebook cell.
-    plt.show()
-
-def plot_image(image):
-    plt.imshow(image.reshape(img_shape),
-               interpolation='nearest',
-               cmap='gray')
-    plt.show()
-
-
-
-
-#image1 = X_train[0]
-#plot_image(np.abs(np.fft.ifft2(np.reshape((image1[0:num_classes:1]),(img_size,img_size)) + np.reshape((image1[0:num_classes:1]),(img_size,img_size))*1j )))
-#
-#
-#image2 = X_train[1]
-#
-#plot_image(np.abs(np.fft.ifft2(np.reshape((image2[0:num_classes:1]),(img_size,img_size)) + np.reshape((image2[0:num_classes:1]),(img_size,img_size))*1j )))
-#
-#plot_conv_weights(weights=weights_conv1)
-#
-#plot_conv_layer(layer=layer_conv1, image=image1)
-#
-#plot_conv_layer(layer=layer_conv1, image=image2)
-#
-#plot_conv_weights(weights=weights_conv2, input_channel=0)
-#
-#plot_conv_weights(weights=weights_conv2, input_channel=1)
-#
-#plot_conv_layer(layer=layer_conv2, image=image1)
-#
-#plot_conv_layer(layer=layer_conv2, image=image2)
-#
-#plot_conv_layer(layer=layer_conv3, image=image1)
-#
-#plot_conv_layer(layer=layer_conv3, image=image2)
 
 
     
